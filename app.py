4c06dbb9ade4ab5916287e45fa9277bb0911dae1
Rômulo Melo <113184965+romulmelo@users.noreply.github.com>
2024-02-27 20:50:36 +0000

    feat: show ctx request

--- app.py
+++ app.py
@@ -8,3 +8,10 @@ def index():
 @app.route('/user/<name>/<prontuario>/<instituicao>')
 def user(name, prontuario, instituicao):
     return f'<h1>Avaliação contínua: Aula 030</h1><h2>Aluno: {name}</h2><h2>Prontuário: {prontuario}</h2><h2>Instituição: {instituicao}</h2><p><a href="/">Voltar</a></p>'
+
+@app.route('/contextorequisicao')
+def contextorequisicao():
+    user_agent = request.headers.get('User-Agent')
+    remote_ip = request.remote_addr
+    host = request.host
+    return f'<h1>Avaliação contínua: Aula 030</h1><h2>Seu navegador é: {user_agent}</h2><h2>O IP do computador remoto é: {remote_ip}</h2><h2>O host da aplicação é: {host}</h2><p><a href="/">Voltar</a></p>'
